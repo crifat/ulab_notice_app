13928aa9375ce738be43fca7e11b41e379a04cd0
nigh7m4r3 <rifatulchayon@gmail.com>
2014-08-16 14:31:43 +0600

    pg at development :/

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140808184135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notices", force: true do |t|
     t.string   "titile"
     t.string   "description"
